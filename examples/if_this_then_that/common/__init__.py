1f109f97f67d05520390270e14ba99b1e581155e
Michelle Sintov <michelle@anki.com>
2016-11-08 17:26:13 -0800

    COZMO-6192 If This Then That examples (#24)
    Gmail, Stocks and Sports IFTTT (If This Then That) examples using aiohttp (asyncio compatible HTTP server)
    Documentation is included in each example on how to configure the associated action on the IFTTT website
    Gmail example: On receipt of an email, play an animation, announce the sender's name, and display an email icon on Cozmo's face
    Sports example: On alert, play an animation, read the alert and display a sports icon on Cozmo's face
    Stock example: On a stock rising more than 1%, play an animation, read the name and schange and display a stock icon on Cozmo's face
    
    Note: There is also a Flask (a non-asyncio-compatible web server) version of the Sports example using a thread and a work queue instead

---
+++ examples/if_this_then_that/common/__init__.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2016 Anki, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License in the file LICENSE.txt or at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+'''If This Then That helper common code.
+
+This module include a subclass of Robot to add some helper methods that are
+useful to the IFTTT examples.
+
+This class includes the following:
+    1) get_in_position moves Cozmo's lift down and face up if necessary.
+    2) display_image_on_face displays the requested image file on his face after
+        the If This Then That trigger has been received.
+'''
+
+import sys
+
+import cozmo
+
+
+try:
+    from PIL import Image, ImageDraw, ImageFont
+except ImportError:
+    sys.exit("Cannot import from PIL: Do `pip3 install --user Pillow` to install")
+
+
+class IFTTTRobot(cozmo.robot.Robot):
+    '''Add some methods to the base Robot class.'''
+    async def get_in_position(self):
+        '''If necessary, Move Cozmo's Head and Lift to make it easy to see Cozmo's face'''
+        if (self.lift_height.distance_mm > 45) or (self.head_angle.degrees < 40):
+            async with self.perform_off_charger():
+                await self.set_lift_height(0.0).wait_for_completed()
+                await self.set_head_angle(cozmo.robot.MAX_HEAD_ANGLE).wait_for_completed()
+
+    def display_image_file_on_face(self, image_name):
+        # load image and convert it for display on cozmo's face
+        image = Image.open(image_name)
+
+        # resize to fit on Cozmo's face screen
+        resized_image = image.resize(cozmo.oled_face.dimensions(), Image.NEAREST)
+
+        # convert the image to the format used by the oled screen
+        face_image = cozmo.oled_face.convert_image_to_screen_data(resized_image,
+                                                                  invert_image=True)
+
+        # display image for 5 seconds
+        self.display_oled_face_image(face_image, 5000.0)
---
+++ examples/if_this_then_that/ifttt_sports.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2016 Anki, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License in the file LICENSE.txt or at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+'''"If This Then That" sports example
+
+This example demonstrates how "If This Then That" (http://ifttt.com) can be used
+make Cozmo respond when there is an in-game or final score update for the team
+you specify. Instructions below will lead you through setting up an applet on
+the IFTTT website. When the applet trigger is called (which sends a web request
+received by the web server started in this example), Cozmo will play an animation,
+show an image on his face, and speak the in-game update.
+
+Please place Cozmo on the charger for this example. When necessary, he will be
+rolled off and back on.
+
+Follow these steps to set up and run the example:
+    1) Provide a a static ip, URL or similar that can be reached from the "If This
+        Then That" server. One easy way to do this is with ngrok, which sets up
+        a secure tunnel to localhost running on your machine.
+
+        To set up ngrok:
+        a) Follow instructions here to download and install:
+            https://ngrok.com/download
+        b) Run this command to create a secure public URL for port 8080:
+            ./ngrok http 8080
+        c) Note the HTTP forwarding address shown in the terminal (e.g., http://55e57164.ngrok.io).
+            You will use this address in your applet, below.
+
+        WARNING: Using ngrok exposes your local web server to the internet. See the ngrok
+        documentation for more information: https://ngrok.com/docs
+
+    2) Set up your applet on the "If This Then That" website.
+        a) Sign up and sign into https://ifttt.com
+        b) Create an applet: https://ifttt.com/create
+        c) Set up your trigger.
+            1. Click "this".
+            2. Select "ESPN" as your service.
+            3. Under "Choose a Trigger", select “New in-game update".
+            4. In section "Complete Trigger Fields", enter your sport and team and click “Create Trigger".
+
+        d) Set up your action.
+            1. Click “that".
+            2. Select “Maker" to set it as your action channel. Connect to the Maker channel if prompted.
+            3. Click “Make a web request" and fill out the fields as follows. Remember your publicly
+                accessible URL from above (e.g., http://55e57164.ngrok.io) and use it in the URL field,
+                followed by "/iftttSports" as shown below:
+
+                 URL: http://55e57164.ngrok.io/iftttSports
+                 Method: POST
+                 Content Type: application/json
+                 Body: {"AlertBody":"{{AlertBody}}"}
+
+            5. Click “Create Action" then “Finish".
+
+    3) Test your applet.
+        a) Run this script at the command line: ./ifttt_sports.py
+        b) On ifttt.com, on your applet page, click “Check now”. See that IFTTT confirms that the applet
+            was checked.
+        c) Wait for new in-game updates for your team and see Cozmo react! Cozmo should roll off the charger, raise
+            and lower his lift, show an image on his face and speak the in-game update.
+'''
+
+import asyncio
+import sys
+
+
+try:
+    from aiohttp import web
+except ImportError:
+    sys.exit("Cannot import from aiohttp. Do `pip3 install --user aiohttp` to install")
+
+import cozmo
+
+from common import IFTTTRobot
+
+
+app = web.Application()
+
+
+async def serve_sports(request):
+    '''Define an HTTP POST handler for receiving requests from If This Then That.
+
+    You may modify this method to change how Cozmo reacts to
+    an in-game update from IFTTT.
+    '''
+
+    json_object = await request.json()
+
+    # Extract the text for the in-game update.
+    alert_body = json_object["AlertBody"]
+
+    robot = request.app['robot']
+    async def read_name():
+        try:
+            async with robot.perform_off_charger():
+                '''If necessary, Move Cozmo's Head and Lift to make it easy to see Cozmo's face.'''
+                await robot.get_in_position()
+
+                # First, have Cozmo play animation "ID_pokedB", which tells
+                # Cozmo to raise and lower his lift. To change the animation,
+                # you may replace "ID_pokedB" with another animation. Run
+                # remote_control_cozmo.py to see a list of animations.
+                await robot.play_anim(name='ID_pokedB').wait_for_completed()
+
+                # Next, have Cozmo speak the text from the in-game update.
+                await robot.say_text(alert_body).wait_for_completed()
+
+                # Last, have Cozmo display a sports image on his face.
+                robot.display_image_file_on_face("../images/ifttt_sports.png")
+
+        except cozmo.RobotBusy:
+            cozmo.logger.warn("Robot was busy so didn't read update: '" + alert_body +"'")
+
+    # Perform Cozmo's task in the background so the HTTP server responds immediately.
+    asyncio.ensure_future(read_name())
+
+    return web.Response(text="OK")
+
+# Attach the function as an HTTP handler.
+app.router.add_post('/iftttSports', serve_sports)
+
+
+if __name__ == '__main__':
+    cozmo.setup_basic_logging()
+    cozmo.robot.Robot.drive_off_charger_on_connect = False
+
+    # Use our custom robot class with extra helper methods
+    cozmo.conn.CozmoConnection.robot_factory = IFTTTRobot
+
+    try:
+        sdk_conn = cozmo.connect_on_loop(app.loop)
+        # Wait for the robot to become available and add it to the app object.
+        app['robot'] = app.loop.run_until_complete(sdk_conn.wait_for_robot())
+    except cozmo.ConnectionError as e:
+        sys.exit("A connection error occurred: %s" % e)
+
+    web.run_app(app)
