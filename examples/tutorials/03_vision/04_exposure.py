35c4087329d5508cb684c18c57fff11ebf62cb98
Mark Wesley <mwesley@anki.com>
2017-03-27 12:06:19 -0700

    COZMO-10267 Moved CameraConfig into Camera (#91)
    Moved CameraConfig from roboy.py into camera.py
    Deprecated robot.camera_config, robot.enable_auto_exposure and robot.set_manual_exposure
    Updated 04_exposure example to match, and made it run once in grayscale and once in color.
    Fixed a couple of small docstring issues in robot.py whilst getting the camera doc changes to work.
    Use pre-chosen fixed values for gain/exposure when modifying the other in the example - gives a better experience across a wider variety of lighting environments.
    Let Cozmo stay on the charger

--- examples/tutorials/03_vision/04_exposure.py
+++ examples/tutorials/03_vision/04_exposure.py
@@ -46,24 +46,26 @@ def camera_info(image, scale, annotator=None, world=None, **kw):
     d = ImageDraw.Draw(image)
     bounds = [3, 0, image.width, image.height]
 
-    camera_config = world.robot.camera_config
+    camera = world.robot.camera
     text_to_display = "Example Mode: " + example_mode + "\n\n"
     text_to_display += "Fixed Camera Settings (Calibrated for this Robot):\n\n"
-    text_to_display += 'focal_length: %s\n' % camera_config.focal_length
-    text_to_display += 'center: %s\n' % camera_config.center
-    text_to_display += 'fov: <%.3f, %.3f> degrees\n' % (camera_config.fov_x.degrees,
-                                                        camera_config.fov_y.degrees)
+    text_to_display += 'focal_length: %s\n' % camera.config.focal_length
+    text_to_display += 'center: %s\n' % camera.config.center
+    text_to_display += 'fov: <%.3f, %.3f> degrees\n' % (camera.config.fov_x.degrees,
+                                                        camera.config.fov_y.degrees)
     text_to_display += "\n"
     text_to_display += "Valid exposure and gain ranges:\n\n"
-    text_to_display += 'exposure: %s..%s\n' % (camera_config.min_exposure_time_ms,
-                                               camera_config.max_exposure_time_ms)
-    text_to_display += 'gain: %.3f..%.3f\n' % (camera_config.min_camera_gain,
-                                               camera_config.max_camera_gain)
+    text_to_display += 'exposure: %s..%s\n' % (camera.config.min_exposure_time_ms,
+                                               camera.config.max_exposure_time_ms)
+    text_to_display += 'gain: %.3f..%.3f\n' % (camera.config.min_gain,
+                                               camera.config.max_gain)
     text_to_display += "\n"
     text_to_display += "Current settings:\n\n"
-    text_to_display += 'Auto Exposure Enabled: %s\n' % camera_config.is_auto_exposure_enabled
-    text_to_display += 'Exposure: %s ms\n' % camera_config.exposure_ms
-    text_to_display += 'Gain: %.3f\n' % camera_config.camera_gain
+    text_to_display += 'Auto Exposure Enabled: %s\n' % camera.is_auto_exposure_enabled
+    text_to_display += 'Exposure: %s ms\n' % camera.exposure_ms
+    text_to_display += 'Gain: %.3f\n' % camera.gain
+    color_mode_str = "Color" if camera.color_image_enabled else "Grayscale"
+    text_to_display += 'Color Mode: %s\n' % color_mode_str
 
     text = cozmo.annotate.ImageText(text_to_display,
                                     position=cozmo.annotate.TOP_LEFT,
@@ -73,37 +75,48 @@ def camera_info(image, scale, annotator=None, world=None, **kw):
     text.render(d, bounds)
 
 
-def cozmo_program(robot: cozmo.robot.Robot):
+def demo_camera_exposure(robot: cozmo.robot.Robot):
     global example_mode
-    robot.world.image_annotator.add_annotator('camera_info', camera_info)
 
     # Ensure camera is in auto exposure mode and demonstrate auto exposure for 5 seconds
-    robot.enable_auto_exposure()
+    camera = robot.camera
+    camera.enable_auto_exposure()
     example_mode = "Auto Exposure"
     time.sleep(5)
 
-    # Grab the current auto exposure/gain values as good defaults for
-    # the current lighting conditions
-    cam = robot.camera_config
-    auto_exposure_ms = cam.exposure_ms
-    auto_gain = cam.camera_gain
-
-    # Demonstrate manual exposure, linearly increasing the exposure time
+    # Demonstrate manual exposure, linearly increasing the exposure time, while
+    # keeping the gain fixed at a medium value.
     example_mode = "Manual Exposure - Increasing Exposure, Fixed Gain"
-    for exposure in range(cam.min_exposure_time_ms, cam.max_exposure_time_ms+1, 1):
-        robot.set_manual_exposure(exposure, auto_gain)
+    fixed_gain = (camera.config.min_gain + camera.config.max_gain) * 0.5
+    for exposure in range(camera.config.min_exposure_time_ms, camera.config.max_exposure_time_ms+1, 1):
+        camera.set_manual_exposure(exposure, fixed_gain)
         time.sleep(0.1)
 
-    # Demonstrate manual exposure, linearly increasing the gain
+    # Demonstrate manual exposure, linearly increasing the gain, while keeping
+    # the exposure fixed at a relatively low value.
     example_mode = "Manual Exposure - Increasing Gain, Fixed Exposure"
-    for gain in np.arange(cam.min_camera_gain, cam.max_camera_gain, 0.05):
-        robot.set_manual_exposure(auto_exposure_ms, gain)
+    fixed_exposure_ms = 10
+    for gain in np.arange(camera.config.min_gain, camera.config.max_gain, 0.05):
+        camera.set_manual_exposure(fixed_exposure_ms, gain)
         time.sleep(0.1)
 
-    # Switch back to auto exposure, demo for a final 5 seconds and then exit
-    robot.enable_auto_exposure()
+    # Switch back to auto exposure, demo for a final 5 seconds and then return
+    camera.enable_auto_exposure()
     example_mode = "Mode: Auto Exposure"
     time.sleep(5)
 
 
+def cozmo_program(robot: cozmo.robot.Robot):
+    robot.world.image_annotator.add_annotator('camera_info', camera_info)
+
+    # Demo with default grayscale camera images
+    robot.camera.color_image_enabled = False
+    demo_camera_exposure(robot)
+
+    # Demo with color camera images
+    robot.camera.color_image_enabled = True
+    demo_camera_exposure(robot)
+
+
+cozmo.robot.Robot.drive_off_charger_on_connect = False  # Cozmo can stay on his charger for this example
 cozmo.run_program(cozmo_program, use_viewer=True, force_viewer_on_top=True)
