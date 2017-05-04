f0173512f89dde77cb52baf82e2a1f26874ff7d2
Mark Wesley <mwesley@anki.com>
2017-05-04 16:12:15 -0700

    COZMO-11126 Remove ID_pokedB anim usage
    ID_pokedB anim was renamed in the 1.5 app
    For future proofing references to it have been changed to use a trigger rather than rely on the exact anim name

--- examples/if_this_then_that/ifttt_sports_flask.py
+++ examples/if_this_then_that/ifttt_sports_flask.py
@@ -145,11 +145,8 @@ def then_that_action(alert_body):
             '''If necessary, Move Cozmo's Head and Lift to make it easy to see Cozmo's face.'''
             robot.get_in_position()
 
-            # First, have Cozmo play animation "ID_pokedB", which tells
-            # Cozmo to raise and lower his lift. To change the animation,
-            # you may replace "ID_pokedB" with another animation. Run
-            # remote_control_cozmo.py to see a list of animations.
-            robot.play_anim(name='ID_pokedB').wait_for_completed()
+            # First, have Cozmo play an animation
+            robot.play_anim_trigger(cozmo.anim.Triggers.ReactToPokeStartled).wait_for_completed()
 
             # Next, have Cozmo speak the text from the in-game update.
             robot.say_text(alert_body).wait_for_completed()
