a9b422dc211a302a339c62c09ae473ad9075f3d3
Shawn Blakesley <shawnblakesley@gmail.com>
2017-11-15 16:46:23 -0800

    COZMO-6191 Fix cli.py when 3d viewer disabled (#25)

--- examples/apps/cli.py
+++ examples/apps/cli.py
@@ -44,7 +44,7 @@ ipyshell = InteractiveShellEmbed(banner1='\nWelcome to the Cozmo Shell',
 def cozmo_program(robot: cozmo.robot.Robot):
     '''Invoke the ipython shell while connected to cozmo'''
     default_log_level = cozmo.logger.level
-    cozmo.logger.level = cozmo.logger.WARN
+    cozmo.logger.setLevel('WARN')
     ipyshell(usage)
     cozmo.logger.level = default_log_level
 
